1eb784f4eb475302c420490618bcbc9fd0b76817
Vanessa <vanessa.hernandez.csu@gmail.com>
2025-03-08 22:40:12 -0800

    Update Addition_Calculator.cpp
    Added multiple operations such as Addition, multiply, subtract, divide as an option ion the calculator updating it from just the addition part

--- Addition_Calculator.cpp
+++ Addition_Calculator.cpp
@@ -10,8 +10,28 @@ int main() {
     std::cout << "Second #: ";
     std::cin >> Second_num;
 
-    Final_Result = First_num + Second_num;
-    std::cout << "Sum: " << Final_Result << std::endl;
+    std::cout << "Operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    if (operation == '+') {
+        Final_Result = First_num + Second_num;
+        std::cout << "Sum: " << Final_Result << std::endl;
+    } else if (operation == '-') {
+        Final_Result = First_num - Second_num;
+        std::cout << "Difference: " << Final_Result << std::endl;
+    } else if (operation == '*') {
+        Final_Result = First_num * Second_num;
+        std::cout << "Product: " << Final_Result << std::endl;
+    } else if (operation == '/') {
+        if (Second_num != 0) {
+            Final_Result = First_num / Second_num;
+            std::cout << "Quotient: " << Final_Result << std::endl;
+        } else {
+            std::cout << "Error: Division by zero is not allowed." << std::endl;
+        }
+    } else {
+        std::cout << "Invalid operation!" << std::endl;
+    }
     
     return 0;
 }
